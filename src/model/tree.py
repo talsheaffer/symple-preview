ab0f94fde9cc5f418c500186243bbef5aadb12c8
Gal Vinograd <gal.vinograd@gmail.com>
2024-09-04 18:39:52 +0300

    rename token tree to expr tree + add expand

--- src/model/tree.py
+++ src/model/tree.py
@@ -23,9 +23,9 @@ TYPE_LIST = [
 
 ARG_NULL = 0
 
-Token = namedtuple('Token', ['type', 'arg'])
+ExprNode = namedtuple('Node', ['type', 'arg'])
 
-class Token(object):
+class ExprNode(object):
     def __init__(self, type: int, arg: int) -> None:
         self.type = type
         self.arg = arg
@@ -37,26 +37,51 @@ class Token(object):
         return tensor
 
     @staticmethod
-    def from_sympy(expr: sp.Expr) -> Token:
+    def from_sympy(expr: sp.Expr) -> ExprNode:
         if isinstance(expr, sp.Add):
-            return Token(ADD_TYPE, ARG_NULL)
+            return ExprNode(ADD_TYPE, ARG_NULL)
         elif isinstance(expr, sp.Mul):
-            return Token(MUL_TYPE, ARG_NULL)
+            return ExprNode(MUL_TYPE, ARG_NULL)
         elif isinstance(expr, sp.Pow):
-            return Token(POW_TYPE, ARG_NULL)
+            return ExprNode(POW_TYPE, ARG_NULL)
         elif isinstance(expr, sp.Integer):
-            return Token(INT_NE_TYPE if expr < 0 else INT_PO_TYPE, expr.args[0])
+            return ExprNode(INT_NE_TYPE if expr < 0 else INT_PO_TYPE, int(expr))
         else:
             raise NotImplementedError(f'Unsupported expression type {type(expr)}')
 
 
-class BinaryTokenTree(object):
-    def __init__(self, root: Token, a: Token, b: Token) -> None:
+class ExprBinaryTree(object):
+    def __init__(self, root: ExprNode, a: ExprNode, b: ExprNode) -> None:
         self.root = root
         self.a = a
         self.b = b
 
-    def to_sorted_list(self) -> list:
+    def expand_b(self) -> "ExprBinaryTree":
+        r = self.root
+        
+        if r.type != MUL_TYPE:
+            return self
+        a, b = self.a, self.b
+        
+        if b.type != ADD_TYPE:
+            return self
+        ba, bb = b.a, b.b
+
+        return ExprBinaryTree(
+            root=ExprNode(ADD_TYPE, ARG_NULL),
+            a=ExprBinaryTree(
+                root=ExprNode(MUL_TYPE, ARG_NULL),
+                a=a,
+                b=ba,
+            ),
+            b=ExprBinaryTree(
+                root=ExprNode(MUL_TYPE, ARG_NULL),
+                a=a,
+                b=bb,
+            ),
+        )
+
+    def topological_sort(self) -> list:
         if self.a is None and self.b is None:
             return [self.root]
         elif self.b is None:
@@ -65,8 +90,8 @@ class BinaryTokenTree(object):
             return self.a.to_list() + self.b.to_list() + [self.root]
 
     @classmethod
-    def from_sympy(cls, expr: sp.Expr) -> "BinaryTokenTree":
-        root = Token.from_sympy(expr)
+    def from_sympy(cls, expr: sp.Expr) -> "ExprBinaryTree":
+        root = ExprNode.from_sympy(expr)
         if isinstance(expr, (sp.Add, sp.Mul)):
             a = cls.from_sympy(expr.args[0])
             b = cls.from_sympy(
@@ -74,19 +99,19 @@ class BinaryTokenTree(object):
                 if len(expr.args) > 2 
                 else expr.args[1]
             )
-            return BinaryTokenTree(
+            return ExprBinaryTree(
                 root=root, 
                 a=cls.from_sympy(a),
                 b=cls.from_sympy(b),
             )
         elif isinstance(expr, sp.Pow):
-            return BinaryTokenTree(
+            return ExprBinaryTree(
                 root=root,
                 a=cls.from_sympy(expr.args[0]),
                 b=cls.from_sympy(expr.args[1]),
             )
         elif isinstance(expr, sp.Integer):
-            return BinaryTokenTree(
+            return ExprBinaryTree(
                 root=root, 
                 a=None, 
                 b=None,
