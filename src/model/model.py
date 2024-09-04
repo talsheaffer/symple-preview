6f56f7a6ff4e30eb7028672d23cb93a3738e2c85
Gal Vinograd <gal.vinograd@gmail.com>
2024-09-04 18:39:52 +0300

    rename token tree to expr tree + add expand

--- src/model/model.py
+++ src/model/model.py
@@ -5,7 +5,7 @@ import torch
 from torch import nn
 
 from model.model_default import DEFAULT_DEVICE, DEFAULT_DTYPE
-from model.tokenizer import BinaryTokenTree, Token
+from src.model.tree import ExprBinaryTree, ExprNode
 
 
 class BinaryTokenTreeModel(nn.Module):
@@ -13,7 +13,7 @@ class BinaryTokenTreeModel(nn.Module):
         super().__init__()
 
         self.embedding = nn.Embedding(
-            num_embeddings=len(Token.TYPE_LIST),
+            num_embeddings=len(ExprNode.TYPE_LIST),
             embedding_dim=embedding_dim,
         )
         self.lstm = nn.LSTM(
@@ -22,7 +22,7 @@ class BinaryTokenTreeModel(nn.Module):
             num_layers=1,
         )
     
-    def forward(self, tree: BinaryTokenTree) -> Dict[Token, torch.Tensor]:
+    def forward(self, tree: ExprBinaryTree) -> Dict[ExprNode, torch.Tensor]:
         out_map = dict()
         hc_map = dict()
 
@@ -33,7 +33,7 @@ class BinaryTokenTreeModel(nn.Module):
         )
         c0 = h0.clone()
 
-        for token in tree.to_sorted_list():
+        for token in tree.topological_sort():
             embedded_token = self.embedding(token.to_tensor())
             ah, ac = hc_map.get(token.a, (h0, c0))
             bh, bc = hc_map.get(token.b, (h0, c0))
