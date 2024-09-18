Debugged node counting in train.py. Fixed sign error.

src/model/environment.py
@@ -34,24 +34,31 @@ OPS_MAP.append(op_finish)
 
 OP_MOVE_UP = next(it)
 def op_move_up(expr: ExprNode, coord: tuple[int, ...]) -> Tuple[ExprNode, tuple[int, ...], int]:
-    new_coord = coord[:-1] if coord else coord
-    return expr, new_coord, 0
+    if coord:
+        new_coord = coord[:-1]
+        return expr, new_coord, 0
+    else:
+        raise ValueError("Can't move up from root")
 OPS_MAP.append(op_move_up)
 
 OP_MOVE_LEFT = next(it)    
 def op_move_left(expr: ExprNode, coord: tuple[int, ...]) -> Tuple[ExprNode, tuple[int, ...], int]:
     new_coord = coord + (0,)
+    if expr.get_node(new_coord) is None:
+        raise ValueError("Can't move left from leaf")
     return expr, new_coord, 0
 OPS_MAP.append(op_move_left)
 
 OP_MOVE_RIGHT = next(it)
 def op_move_right(expr: ExprNode, coord: tuple[int, ...]) -> Tuple[ExprNode, tuple[int, ...], int]:
     new_coord = coord + (1,)
+    if expr.get_node(new_coord) is None:
+        raise ValueError("Can't move right from leaf")
     return expr, new_coord, 0
 OPS_MAP.append(op_move_right)
 
 OP_COMMUTE = next(it)
-@apply_op_and_count
+@apply_op_for_no_change
 def op_commute(expr: ExprNode) -> ExprNode:
     return expr.commute()
 OPS_MAP.append(op_commute)
@@ -104,10 +111,11 @@ class Symple:
         self,
         time_penalty: float = -0.02,
         node_count_importance_factor: float = 1.0,
-        compute_penalty_coefficient: float = 1e-6,
+        compute_penalty_coefficient: float = 1e-7,
         min_steps: int = 0,
         max_steps: int = 1000,
     ):
+        assert time_penalty <= 0, "Time penalty must be non-positive"
         self.time_penalty = time_penalty
         self.node_count_importance_factor = node_count_importance_factor
         self.min_steps = min_steps
@@ -115,17 +123,18 @@ class Symple:
         self.max_steps = max_steps
         self.num_ops = NUM_OPS
     def step(self, expr: ExprNode, current_coord: tuple[int, ...], action: int) -> Tuple[ExprNode, tuple[int, ...], float, bool]:
-        reward = self.time_penalty if self.min_steps == 0 else 0.
+        if  (action == OP_FINISH and self.min_steps > 0):
+            vm = self.get_validity_mask(expr, current_coord)
+            assert (torch.nn.functional.one_hot(torch.tensor(action),num_classes = self.num_ops) == vm).all().item(), "Invalid action"
         
         new_expr, new_coord, node_count_reduction = OPS_MAP[action](expr, current_coord)
         
-        reward += self.node_count_importance_factor * node_count_reduction
+        reward = self.time_penalty + self.node_count_importance_factor * node_count_reduction
         
         if self.max_steps > 0:
             self.max_steps -= 1
         if self.min_steps > 0:
             self.min_steps -= 1
-        
         
         done = action == OP_FINISH or self.max_steps == 0
         

src/model/model.py
@@ -194,7 +194,7 @@ class SympleAgent(nn.Module):
         
         # Add reward and coordinates to internal and high-level action history
         for entry in history:
-            entry['reward'] = env.time_penalty - (env.compute_penalty_coefficient * entry['complexity'])
+            entry['reward'] = env.time_penalty  - env.compute_penalty_coefficient * entry['complexity']
             entry['coordinates'] = coord
         
         history.append({

train/train.py
@@ -62,7 +62,7 @@ agent = SympleAgent(
 optimizer = torch.optim.Adam(agent.parameters(), lr=0.001)
 
 # Training loop
-num_epochs = 10
+num_epochs = 2
 batch_size = 32
 
 
@@ -89,7 +89,7 @@ for epoch in range(1, num_epochs + 1):
         avg_return, batch_history, output_expr_nodes = train_on_batch(agent, batch, optimizer,
                                     behavior_policy=behavior_policy,
                                     **dict(
-                                        time_penalty=0.02,
+                                        # time_penalty=-0.02,
                                         min_steps=30,
                                     )
                                 )
@@ -107,14 +107,16 @@ for epoch in range(1, num_epochs + 1):
         print(f"Epoch {epoch}/{num_epochs}, Batch {batch_number}/{n_batches}, Return: {avg_return:.4f}, NCR: {avg_ncr:.4f}, Batch Time: {batch_time:.4f} seconds, Avg Eval Time: {avg_eval_time:.4f} seconds")
 
         # Compute and verify node count reduction
-        for input_expr, output_expr, history in zip(batch, output_expr_nodes, batch_history):
+        for input_expr, output_expr, history in zip(
+            shuffled_data[i:i+batch_size].apply(ExprNode.from_sympy).tolist(), output_expr_nodes, batch_history
+        ):
             input_node_count = input_expr.node_count()
             output_node_count = output_expr.node_count()
             computed_ncr = input_node_count - output_node_count
             history_ncr = sum([step['node_count_reduction'] for step in history])
             
-            # if computed_ncr != history_ncr:
-            #     print(f"Warning: Computed NCR ({computed_ncr}) doesn't match history NCR ({history_ncr})")
+            assert computed_ncr == history_ncr, f"Warning: Computed NCR ({computed_ncr}) doesn't match history NCR ({history_ncr})"
+                # print(f"Warning: Computed NCR ({computed_ncr}) doesn't match history NCR ({history_ncr})")
                 # print(f"Input expression: {input_expr}")
                 # print(f"Output expression: {output_expr}")
 