950dbfbb6b1386037d95bed1d224a8e91e79721b
Tal Sheaffer <sheaffertal@gmail.com>
2024-09-25 03:44:34 +0300

    Implemented topology change handling using wrapper.

--- src/model/actions.py
+++ src/model/actions.py
@@ -1,28 +1,41 @@
-from symple.expr.actions import ACTIONS as ACTIONS_BASE, Action
-from typing import Tuple
+from symple.expr.actions import ACTIONS, ActionType
+from symple.expr.expr_node import ExprNode as ExprNodeBase
+from typing import Tuple, Callable
 from src.model.tree import ExprNode
 
-def wrap_action(action):
-    def wrapper(en):
-        return ExprNode.from_expr_node_base(action(en))
-    return wrapper
+depths = {key: 1 for key in ACTIONS.keys()}
+depths[ActionType.ASSOCIATE_LEFT] = 2
+depths[ActionType.ASSOCIATE_RIGHT] = 2
+depths[ActionType.DISTRIBUTE_LEFT] = 2
+depths[ActionType.DISTRIBUTE_RIGHT] = 2
+depths[ActionType.FACTOR_RIGHT] = 2
+depths[ActionType.FACTOR_LEFT] = 2
+depths[ActionType.REDUCE_DOUBLE_INV] = 0
+depths[ActionType.REDUCE_GROUP_UNIT] = 0
 
-ACTIONS = {
-    action_type: Action(action.can_apply, wrap_action(action.apply))
-    for action_type, action in ACTIONS_BASE.items()
-}
 
-def apply_op_and_count(op_func):
-    def wrapper(expr: ExprNode, coord: tuple[int, ...] = ()) -> Tuple[ExprNode, tuple[int, ...], int]:
+
+def wrap_action(
+        action: Callable[[ExprNodeBase], ExprNodeBase],
+        depth: int = 0
+) -> Callable[[ExprNodeBase, tuple[int, ...]], Tuple[ExprNode, tuple[int, ...], int, int]]:
+    
+    def subclass_wrapper(expr: ExprNodeBase) -> ExprNode:
+        return ExprNode.from_expr_node_base(action(expr))
+    
+    def wrapper(expr: ExprNodeBase, coord: tuple[int, ...] = ()) -> Tuple[ExprNode, tuple[int, ...], int]:
         initial_count = expr.get_node(coord).node_count()
-        new_expr = expr.apply_at_coord(coord, op_func)
+        nodes = expr.get_node(coord).topological_sort(depth)
+        tensors = [(n.hidden, n.cell) for n in nodes]
+        new_expr = expr.apply_at_coord(coord, subclass_wrapper)
         final_count = new_expr.get_node(coord).node_count()
         reduction = initial_count - final_count
-        return new_expr, coord, reduction
+        nodes = new_expr.get_node(coord).topological_sort(depth)
+        for i, n in enumerate(nodes):
+            n.hidden = tensors[i][0]
+            n.cell = tensors[i][1]
+        return new_expr, coord, reduction, depth
     return wrapper
 
-ACTIONS = {
-    action_type: Action(action.can_apply, apply_op_and_count(action.apply))
-    for action_type, action in ACTIONS.items()
-}
-
+for action_type, action in ACTIONS.items():
+    action.apply = wrap_action(action.apply, depth = depths[action_type])
