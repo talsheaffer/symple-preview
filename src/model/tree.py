40774561c98c1429c64f63179aa42d4a3356853d
Tal Sheaffer <sheaffertal@gmail.com>
2024-09-29 00:17:23 +0300

    Added sympy functions

--- src/model/tree.py
+++ src/model/tree.py
@@ -139,17 +139,17 @@ class ExprNode(ExprNodeBase):
     def from_sympy(cls, expr: sp.Expr) -> "ExprNode":
         return cls.from_expr_node_base(ExprNodeBase.from_sympy(expr))
     
-    def to_sympy(self) -> sp.Expr:
+    def to_sympy(self, evaluate: bool = True) -> sp.Expr:
         if self.type == ExprNodeType.ADD:
-            return sp.Add(self.left.to_sympy(), self.right.to_sympy(), evaluate=True)
+            return sp.Add(self.left.to_sympy(evaluate), self.right.to_sympy(evaluate), evaluate=evaluate)
         elif self.type == ExprNodeType.NEG:
-            return sp.Mul(sp.Integer(-1), self.left.to_sympy(), evaluate=True)
+            return sp.Mul(sp.Integer(-1), self.left.to_sympy(evaluate), evaluate=evaluate)
         elif self.type == ExprNodeType.MUL:
-            return sp.Mul(self.left.to_sympy(), self.right.to_sympy(), evaluate=True)
+            return sp.Mul(self.left.to_sympy(evaluate), self.right.to_sympy(evaluate), evaluate=evaluate)
         elif self.type == ExprNodeType.INV:
-            return sp.Pow(self.left.to_sympy(), sp.Integer(-1), evaluate=True)
+            return sp.Pow(self.left.to_sympy(evaluate), sp.Integer(-1), evaluate=evaluate)
         elif self.type == ExprNodeType.POW:
-            return sp.Pow(self.left.to_sympy(), self.right.to_sympy(), evaluate=True)
+            return sp.Pow(self.left.to_sympy(evaluate), self.right.to_sympy(evaluate), evaluate=evaluate)
         elif self.type == ExprNodeType.INT:
             return sp.Integer(self.arg)
         elif self.type == ExprNodeType.VAR_X:
