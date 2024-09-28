a8595af3295145d55bc8143b0a8b537d91855778
Tal Sheaffer <sheaffertal@gmail.com>
2024-09-28 15:48:18 +0300

    squashed high-level action. Need to add off policy

--- train/aux_policies.py
+++ train/aux_policies.py
@@ -1,16 +1,15 @@
 import torch
-from src.model.model import ExprNode
+from src.model.model import SympleState
 
-def random_policy(state : ExprNode, validity_mask : torch.Tensor) -> torch.Tensor:
+def random_policy(state: SympleState, validity_mask: torch.Tensor) -> torch.Tensor:
     """
     A random policy that returns equal probabilities for all valid actions.
     
     Args:
-    state (ExprNode): The current state of the environment.
+    state (SympleState): The current state of the environment.
     validity_mask (torch.Tensor): A tensor indicating the validity of actions.
     
     Returns:
     torch.Tensor: A tensor of probabilities for each action.
     """
-    return validity_mask.view((1,len(validity_mask)))/validity_mask.sum()
-
+    return validity_mask.view((1, len(validity_mask))) / validity_mask.sum()
